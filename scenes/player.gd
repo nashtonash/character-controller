73f710c8924ee77aba8f19df7224c905b3a58330
nashtonash <41835529+nashtonash@users.noreply.github.com>
2024-08-29 14:40:25 -0400

    movement and camera 0

--- scenes/player.gd
+++ scenes/player.gd
@@ -1,7 +1,20 @@
 extends CharacterBody3D
 
+var mouse_sensitivity: float = .005
+
+func _input(event: InputEvent) -> void:
+	if (event is InputEventMouseMotion):
+		$CameraPivot.rotate_y(-event.relative.x * mouse_sensitivity)
+		$CameraPivot.rotate_x(-event.relative.y * mouse_sensitivity)
+		
+
 func calculate_velocity() -> void:
 	pass
 	
 func _physics_process(delta: float) -> void:
-	pass
+	var input_dir: Vector2 = Input.get_vector(
+		"move_left",
+		"move_right",
+		"move_forward",
+		"move_back",
+	)
